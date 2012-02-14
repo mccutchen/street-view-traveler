24ffdff2229b032101d012e89d612549956851d5
Will McCutchen <wm@bitly.com>
2012-02-14 15:23:36 -0500

    Tripview now just generates streetview image URLs.
    It will be up to something else (a distributed mapper? a companion
    script?) to actually fetch the images.

--- tripview.py
+++ tripview.py
@@ -1,39 +1,47 @@
+#!/usr/bin/env python
+
 import json
 import math
 import os
+import sys
+import urllib
 
-import requests
 import polyline
 
+
+# All maps requests start here
+MAPS_API_URL = 'http://maps.googleapis.com/maps/api'
 
 # Max distance allowed between points before we do linear interpolation
 MAX_DISTANCE = 0.00025
 
 
-def get_directions(origin, destination):
-    """Fetches the directions between the two addresses. Example URL:
+def get_route(a, b):
+    """Fetches the directions between the two addresses for point A and point
+    B. Example URL:
 
         http://maps.googleapis.com/maps/api/directions/json?
             origin=117+Henry+St,+New+York, NY 10002&
             destination=Los+Angeles,CA&
-            waypoints=Joplin,MO|Oklahoma+City,OK&
             sensor=false
     """
     params = {
-        'origin': origin,
-        'destination': destination,
+        'origin': a,
+        'destination': b,
         'sensor': 'false'
         }
-    return maps_api_request('/directions/json', params)
+    status, body = http_req('GET', MAPS_API_URL + '/directions/json', params)
+    assert 200 <= status <= 299, 'HTTP error: %r: %r' % (status, body)
+    directions = json.loads(body)
+    assert directions['routes'], 'No directions between %r and %r' % (a, b)
+    return directions['routes'][0]
 
-def iter_points(directions, max_dist=0):
-    """A generator that yields each (lat, lon) pair along the given
-    directions.
+def iter_points(route, max_dist=0):
+    """A generator that yields each (lat, lon) pair along the given route.
 
     If max_dist is non-zero, additional points will be linearly-interpolated
     between any two points in the route that are farther than max_dist apart.
     """
-    route = directions['routes'][0]
     prev_point = None
     for leg in route['legs']:
         for step in leg['steps']:
@@ -71,7 +79,7 @@ def get_heading((x1, y1), (x2, y2)):
     angle = math.degrees(math.atan2(x1-x2, y1-y2)) + 90
     return angle + 360 if angle < 0 else angle
 
-def get_frame(point, heading):
+def get_frame_url(point, heading):
     """Fetches a single still for the given point and heading from the
     streetview API. Example URL:
 
@@ -90,31 +98,42 @@ def get_frame(point, heading):
         'size': '600x300',
         'sensor': 'false',
     }
-    return maps_api_request('/streetview', params)
+    return MAPS_API_URL + '/streetview?' + urllib.urlencode(params)
 
-def maps_api_request(path, params={}):
-    """Makes a request to the Google Maps API. Will automatically parse
-    JSON responses.
+def http_req(method, url, params={}):
+    """Makes an HTTP request, returning a 2-tuple of (status, body). Only
+    makes GET and POST requests because a) fuck Python's HTTP libs and b) I
+    didn't want a dependency on the `requests` lib.
     """
-    url = 'http://maps.googleapis.com/maps/api/' + path.lstrip('/')
-    resp = requests.get(url, params=params)
-    assert 200 <= resp.status_code < 300
-    if 'application/json' in resp.headers['Content-Type']:
-        return json.loads(resp.content)
-    return resp.content
+    assert method in ('GET', 'POST'), 'Unsupported HTTP method: %s' % method
+    if method == 'GET' and params:
+        url = url + '?' + urllib.urlencode(params, True)
+        params = None
+    resp = urllib.urlopen(url, data=params)
+    return resp.code, resp.read()
+
+def main(start, end):
+    try:
+        route = get_route(start, end)
+    except Exception, e:
+        print >> sys.stderr, 'Error fetching directions: %s' % e
+        return 1
+    else:
+        prev_point = None
+        for i, point in enumerate(iter_points(route, MAX_DISTANCE)):
+            heading = get_heading(point, prev_point) if prev_point else 0
+            url = get_frame_url(point, heading)
+            print >> sys.stdout, '%s\t%s' % (i, url)
+            prev_point = point
+        return 0
 
 if __name__ == '__main__':
-    os.system('rm -f *.jpg trip.mp4')
-    a = '101 Henry St, New York, NY 10002'
-    b = '171 Henry St, New York, NY 10002'
-    directions = get_directions(a, b)
-    prev_point = None
-    for i, point in enumerate(iter_points(directions, MAX_DISTANCE)):
-        lon, lat = point
-        heading = get_heading(point, prev_point) if prev_point else None
-        print '%s %s (%s)' % (lon, lat, heading)
-        if heading:
-            open('%05d.jpg' % i, 'wb').write(get_frame(point, heading))
-        prev_point = point
-    os.system('/usr/local/bin/ffmpeg -y -i "%05d.jpg" -sameq trip.mp4')
-    os.system('open trip.mp4')
+    if not len(sys.argv) == 3:
+        print >> sys.stderr, """Invalid command. Usage:
+
+    %s start_address end_address
+""" % os.path.basename(sys.argv[0])
+        sys.exit(1)
+    else:
+        start, end = sys.argv[1:]
+        sys.exit(main(start, end))
